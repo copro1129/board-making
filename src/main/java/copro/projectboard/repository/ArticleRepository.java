7f29334d9c02709bfab2ff37a4b4f5769d57dade
copro <hyunsigi42@naver.com>
2022-10-12 17:24:46 +0900

    #1 - data rest 설정
    게시글, 댓글의 json api 를 자동으로
    restful 하게 만들게끔 설정

--- src/main/java/copro/projectboard/repository/ArticleRepository.java
+++ src/main/java/copro/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package copro.projectboard.repository;
 
 import copro.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
