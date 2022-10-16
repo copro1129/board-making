6f6168b650c968f2a749a782f3c9a7f24a87b366
copro <hyunsigi42@naver.com>
2022-10-16 10:20:15 +0900

    #22 - 댓글 기능 구현 및 전체 테스트 동작 확인
    댓글 기능 구현 및 테스트 동작 확인

--- src/main/java/copro/projectboard/service/ArticleCommentService.java
+++ src/main/java/copro/projectboard/service/ArticleCommentService.java
@@ -1,33 +1,56 @@
 package copro.projectboard.service;
 
+import copro.projectboard.domain.ArticleComment;
 import copro.projectboard.dto.ArticleCommentDto;
 import copro.projectboard.repository.ArticleCommentRepository;
 import copro.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 @Transactional
+@Slf4j
 public class ArticleCommentService {
 
+    private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
 
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComments(Long articleId) {
-        return List.of();
+        return articleCommentRepository.findByArticle_Id(articleId)
+                .stream()
+                .map(ArticleCommentDto::from)
+                .toList();
     }
+
     public void saveArticleComment(ArticleCommentDto dto) {
+        try {
+            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
+
     public void updateArticleComment(ArticleCommentDto dto) {
+        try {
+            ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
+            if (dto.content() != null) {
+                articleComment.setContent(dto.content());
+            }
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 업데이트 실패. 댓글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
     public void deleteArticleComment(Long articleCommentId) {
+        articleCommentRepository.deleteById(articleCommentId);
     }
-
 
 
 }
